e5819cda559f4e7ced53fdb1541271c499f1b91f
Asuma Yamada <yamadaasuma@gmail.com>
2024-08-21 03:45:17 -0400

    fix: format

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
-import { Trash2 } from "lucide-react";
-import { Account } from "starknet";
-import Avatar from "./Avatar";
-import { truncateAddress } from "@/utils";
+import { Trash2 } from 'lucide-react'
+import { Account } from 'starknet'
+import Avatar from './Avatar'
+import { truncateAddress } from '@/utils'
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -9,42 +9,45 @@ import {
   DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import { useDojo } from "@/libs/dojo/useDojo";
-import { useMemo } from "react";
-import Spinner from "./Spinner";
+} from '@/components/ui/dropdown-menu'
+import { useDojo } from '@/libs/dojo/useDojo'
+import { useMemo } from 'react'
+import Spinner from './Spinner'
 
 const Header = ({ account }: { account: Account }) => {
   const {
     account: { select, list, create, remove, isDeploying },
-  } = useDojo();
+  } = useDojo()
 
-  const otherBurnerAccounts = useMemo(() => list().filter((a) => a.address !== account.address), [account, list]);
+  const otherBurnerAccounts = useMemo(
+    () => list().filter((a) => a.address !== account.address),
+    [account, list],
+  )
 
   const onSelect = (e: React.MouseEvent<HTMLDivElement>, address: string) => {
-    e.preventDefault();
-    select(address);
-  };
+    e.preventDefault()
+    select(address)
+  }
 
   const onCreate = (e: React.MouseEvent<HTMLDivElement>) => {
-    e.preventDefault();
-    create();
-  };
+    e.preventDefault()
+    create()
+  }
 
   const onDelete = (e: React.MouseEvent<SVGSVGElement>, address: string) => {
-    e.preventDefault();
-    remove(address);
-  };
+    e.preventDefault()
+    remove(address)
+  }
 
   return (
-    <header className="bg-primary h-[50px] w-full flex items-center justify-between p-4">
-      <h1 className="text-white text-lg font-bold">
-        <img src="logo.png" alt="PixeLAW" className="object-contain h-10" />
+    <header className='bg-primary h-[50px] w-full flex items-center justify-between p-4'>
+      <h1 className='text-white text-lg font-bold'>
+        <img src='logo.png' alt='PixeLAW' className='object-contain h-10' />
       </h1>
       <DropdownMenu>
         <DropdownMenuTrigger>
-          <div className="flex items-center space-x-4 border-2 border-slate-600 rounded-sm p-1 px-3">
-            <div className="text-white">{truncateAddress(account.address)}</div>
+          <div className='flex items-center space-x-4 border-2 border-slate-600 rounded-sm p-1 px-3'>
+            <div className='text-white'>{truncateAddress(account.address)}</div>
             <Avatar address={account.address} size={32} />
           </div>
         </DropdownMenuTrigger>
@@ -55,27 +58,27 @@ const Header = ({ account }: { account: Account }) => {
             <DropdownMenuItem
               onClick={(e) => onSelect(e, account.address)}
               key={account.address}
-              className="cursor-pointer flex justify-between"
+              className='cursor-pointer flex justify-between'
             >
               {truncateAddress(account.address)}
-              <Trash2 size={16} color="red" onClick={(e) => onDelete(e, account.address)} />
+              <Trash2 size={16} color='red' onClick={(e) => onDelete(e, account.address)} />
             </DropdownMenuItem>
           ))}
           <DropdownMenuSeparator />
           <DropdownMenuItem onClick={onCreate}>
             {isDeploying ? (
-              <div className="flex items-center space-x-2 justify-between">
-                <Spinner color="black" />
+              <div className='flex items-center space-x-2 justify-between'>
+                <Spinner color='black' />
                 Deploying...
               </div>
             ) : (
-              <div className="flex items-center space-x-2">Create</div>
+              <div className='flex items-center space-x-2'>Create</div>
             )}
           </DropdownMenuItem>
         </DropdownMenuContent>
       </DropdownMenu>
     </header>
-  );
-};
+  )
+}
 
-export { Header };
+export { Header }
--- src/components/PixelViewer/index.tsx
+++ src/components/PixelViewer/index.tsx
@@ -1,11 +1,11 @@
-import React from "react";
-import { COLOR_PALETTE, DEFAULT_BACKGROUND_COLOR, DEFAULT_GRID_COLOR } from "./const";
-import { usePixelViewer } from "./hooks";
-import { type Color } from "./types";
+import React from 'react'
+import { COLOR_PALETTE, DEFAULT_BACKGROUND_COLOR, DEFAULT_GRID_COLOR } from './const'
+import { usePixelViewer } from './hooks'
+import { type Color } from './types'
 
 interface PixelViewerProps {
-  backgroundColor?: Color;
-  gridColor?: Color;
+  backgroundColor?: Color
+  gridColor?: Color
 }
 
 const PixelViewer: React.FC<PixelViewerProps> = ({
@@ -23,14 +23,14 @@ const PixelViewer: React.FC<PixelViewerProps> = ({
     handleMouseMove,
     handleMouseUp,
     handleWheel,
-  } = usePixelViewer(backgroundColor, gridColor);
+  } = usePixelViewer(backgroundColor, gridColor)
 
   return (
-    <div className="relative h-full w-full">
+    <div className='relative h-full w-full'>
       <canvas
         ref={canvasRef}
-        className="fixed inset-x-0 bottom top-[50px]"
-        style={{ width: "100%", height: "calc(100% - 50px)" }}
+        className='fixed inset-x-0 bottom top-[50px]'
+        style={{ width: '100%', height: 'calc(100% - 50px)' }}
         onTouchStart={handleTouchStart}
         onTouchMove={handleTouchMove}
         onTouchEnd={handleTouchEnd}
@@ -39,14 +39,18 @@ const PixelViewer: React.FC<PixelViewerProps> = ({
         onMouseUp={handleMouseUp}
         onWheel={handleWheel}
       />
-      <div className="rounded-md px-4 bg-primary max-w-fit fixed mx-auto bottom-1 left-0 right-0 flex h-[50px] items-center justify-center space-x-8 shadow-md">
-        <div className="flex items-center space-x-2">
+      <div className='rounded-md px-4 bg-primary max-w-fit fixed mx-auto bottom-1 left-0 right-0 flex h-[50px] items-center justify-center space-x-8 shadow-md'>
+        <div className='flex items-center space-x-2'>
           {COLOR_PALETTE.map((color, index) => (
             <button
               key={index}
-              className={`size-8 rounded-full ${selectedColor === color ? "ring-2 ring-black ring-offset-2" : ""}`}
+              className={`size-8 rounded-full ${
+                selectedColor === color ? 'ring-2 ring-black ring-offset-2' : ''
+              }`}
               style={{
-                backgroundColor: `rgba(${color.r * 255}, ${color.g * 255}, ${color.b * 255}, ${color.a})`,
+                backgroundColor: `rgba(${color.r * 255}, ${color.g * 255}, ${color.b * 255}, ${
+                  color.a
+                })`,
               }}
               onClick={() => setSelectedColor(color)}
             />
@@ -54,7 +58,7 @@ const PixelViewer: React.FC<PixelViewerProps> = ({
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export { PixelViewer };
+export { PixelViewer }
