ac53a467b5474622f8cfe3ab93a837d34e960fa7
Asuma Yamada <yamadaasuma@gmail.com>
2024-08-21 03:40:38 -0400

    fix: trycatch

--- src/libs/dojo/createSystemCalls.ts
+++ src/libs/dojo/createSystemCalls.ts
@@ -1,58 +1,55 @@
 /* eslint-disable @typescript-eslint/no-unused-vars -- for now */
-import { Account, type AccountInterface } from 'starknet'
-import { type ClientComponents } from './createClientComponents'
-import { type ContractComponents } from './generated/contractComponents'
-import type { DefaultParams, IWorld, PixelUpdate } from './generated/generated'
+import { Account, type AccountInterface } from "starknet";
+import { type ClientComponents } from "./createClientComponents";
+import { type ContractComponents } from "./generated/contractComponents";
+import type { DefaultParams, IWorld, PixelUpdate } from "./generated/generated";
 
-export type SystemCalls = ReturnType<typeof createSystemCalls>
+export type SystemCalls = ReturnType<typeof createSystemCalls>;
 
 export function createSystemCalls(
   { client }: { client: IWorld },
   contractComponents: ContractComponents,
-  clientComponents: ClientComponents,
+  clientComponents: ClientComponents
 ) {
   const initCore = async (account: Account | AccountInterface) => {
     try {
       await client.actions.initCore({
         account,
-      })
+      });
     } catch (e) {
-      console.error(e)
+      console.error(e);
     }
-  }
+  };
 
   const updatePixel = async (account: Account | AccountInterface, pixelUpdate: PixelUpdate) => {
     try {
-      await client.actions.updatePixel(account, pixelUpdate)
+      await client.actions.updatePixel(account, pixelUpdate);
     } catch (e) {
-      console.error(e)
+      console.error(e);
     }
-  }
+  };
 
   const initPaint = async (account: Account | AccountInterface) => {
     try {
-      await client.actions.initPaint(account)
+      await client.actions.initPaint(account);
     } catch (e) {
-      console.error(e)
+      console.error(e);
     }
-  }
+  };
 
-  const interact = async (
-    account: Account | AccountInterface,
-    params: Pick<DefaultParams, 'x' | 'y' | 'color'>,
-  ) => {
-    console.log('interact', params)
+  const interact = async (account: Account | AccountInterface, params: Pick<DefaultParams, "x" | "y" | "color">) => {
+    console.log("interact", params);
     try {
-      await client.actions.interact(account, { ...params })
+      await client.actions.interact(account, { ...params });
     } catch (e) {
-      console.error(e)
+      console.error(e);
     }
-  }
+  };
 
   return {
     initCore,
     updatePixel,
     initPaint,
     interact,
-  }
+  };
 }
