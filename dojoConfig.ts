c993619eae9dd10454581aa6203b1f51eb388ed3
Asuma Yamada <yamadaasuma@gmail.com>
2024-09-03 18:28:47 -0400

    fix: manifest

--- dojoConfig.ts
+++ dojoConfig.ts
@@ -1,11 +1,11 @@
 import { createDojoConfig } from "@dojoengine/core";
-// import manifestDev from "../core/contracts/manifests/dev/deployment/manifest.json";
-import manifestSlot from "./src/libs/dojo/generated/manifests/deployments/slot/manifest.json";
+// import manifest from "../core/contracts/manifests/dev/deployment/manifest.json";
+import manifest from "./src/libs/dojo/generated/manifests/deployments/slot/manifest.json";
 
 export const dojoConfig = createDojoConfig({
   toriiUrl: import.meta.env.VITE_PUBLIC_TORII_URL,
   rpcUrl: import.meta.env.VITE_PUBLIC_RPC_URL,
   masterAddress: import.meta.env.VITE_PUBLIC_MASTER_ADDRESS,
   masterPrivateKey: import.meta.env.VITE_PUBLIC_MASTER_PRIVATE_KEY,
-  manifest: manifestSlot,
+  manifest: manifest,
 });
