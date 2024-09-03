4ac67cf9d1afa7fc30c5b2ef3ebbfd53193bd7d2
Asuma Yamada <yamadaasuma@gmail.com>
2024-09-02 21:57:54 -0400

    fix: type

--- src/types/index.ts
+++ src/types/index.ts
@@ -13,30 +13,11 @@ export interface Color {
   a: number;
 }
 
-export interface GridDimensions {
-  width: number;
-  height: number;
-}
-
 export interface GridState {
   offsetX: number;
   offsetY: number;
   scale: number;
   lastPinchDist?: number;
-}
-
-export interface ProgramInfo {
-  program: WebGLProgram;
-  attribLocations: {
-    position: number;
-  };
-  uniformLocations: {
-    resolution: WebGLUniformLocation | null;
-    offset: WebGLUniformLocation | null;
-    scale: WebGLUniformLocation | null;
-    color: WebGLUniformLocation | null;
-    lineWidth: WebGLUniformLocation | null;
-  };
 }
 
 export interface Pixel {
