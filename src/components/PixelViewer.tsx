b24d159e1f7e60ce3d278947e6bb3549592b4055
Asuma Yamada <yamadaasuma@gmail.com>
2024-09-16 03:39:16 -0400

    feat: add onTap

--- src/components/PixelViewer.tsx
+++ src/components/PixelViewer.tsx
@@ -111,6 +111,7 @@ export const PixelViewer: React.FC = () => {
         onCellClick={onCellClick}
         onSwipe={onPan}
         onPan={onPan}
+        onTap={onCellClick}
         onDrawGrid={onDrawGrid}
         onGridStateChange={setGridState}
         setCurrentMousePos={setCurrentMousePos}
