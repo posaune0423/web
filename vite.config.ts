7d14a3601b6f0e3874888f6987ece17fdf06c2cb
Asuma Yamada <yamadaasuma@gmail.com>
2024-09-21 04:09:00 -0400

    tweak

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,15 @@ import { viteEnvs } from "vite-envs";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), wasm(), topLevelAwait(), viteEnvs(), glsl()],
+  plugins: [
+    react(),
+    wasm(),
+    topLevelAwait(),
+    viteEnvs({
+      declarationFile: ".env.development",
+    }),
+    glsl(),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
